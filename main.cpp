ab0521f69bf963ae514e36997609c8eebf58b929
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2024-05-01 09:56:24 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 using namespace std;
 
 
+//Question 1
+template <typename T>
+bool isTriangle(T side1, T side2, T side3){
+    if (side1>abs(side2-side3) and side1<side2+side3){
+        return true;
+    }
+    return false;
+}
+
 
 
 int main() {
+
+
+    //Question 1
+    double side1, side2, side3;
+    cin>>side1>>side2>>side3;
+    if (isTriangle(side1, side2, side3)){
+        cout<<"Triangle can be formed with these sides"<<endl;
+    } else{
+        cout<<"Triangle can NOT be formed with these sides"<<endl;
+    }
 
 
 
