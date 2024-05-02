4c12f15a45f5723e044e6cc7291998d5f3704e6e
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2024-05-02 16:07:48 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,21 +15,64 @@ bool isTriangle(T side1, T side2, T side3){
 
 //Question 2
 template <typename T>
-void encrypt(T& msg, int key){
-    int lens=msg.length();
-    for(int i =0; i<lens; i++){
-        if (int(msg[i])!=32){
-            if(int(msg[i])<122 and int(msg[i])+key>122){
-                msg[i]=char((int(msg[i])+key)%122+97-1);
-            }
-            else if (int(msg[i])<90 and int(msg[i])+key>90){
-                msg[i]=char((int(msg[i])+key)%90+65-1);
+void encrypt(T& text, int key){
+    int lens=text.length();
+    key=key%26;
+    int position=0;
+    for (int i=0; i<lens;  i++){
+        if (text[i]!=' '){
+            position=int(text[i])+key;
+            if (position>122){
+                text[i]=position-122+96;
+            } else if (position>'Z' and position-key<'Z'){
+                text[i]=position-90+64;
             } else{
-                msg[i]=char(int(msg[i])+key);
+                text[i]=position;
             }
+
         }
     }
 }
+
+
+
+//Question 3
+template <typename T>
+double sum3(T arr3){
+    double sum=0;
+    for (int i=0; i<7; i++){
+        sum+=arr3[i];
+    }
+    return sum;
+}
+
+
+
+
+//Question 4
+template <typename T>
+double check4(T arr4, double num4){
+    for (int i=0; i<10; i++){
+        if (arr4[i]==num4){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+
+//Question 5
+template <typename T>
+void pow2(T& arr5){
+    for (int i=0; i<5; i++){
+        arr5[i]=pow(arr5[i], 2);
+    }
+}
+
+
 
 
 
@@ -48,12 +91,53 @@ int main() {
 
 
     //Question 2
-    string msg;
+    string text;
     int key;
-    getline(cin, msg);
+    getline(cin, text);
     cin>>key;
-    encrypt(msg, key);
-    cout<<msg<<endl;
+    encrypt(text, key);
+    cout<<text;
+
+
+
+
+    //Question 3
+    double arr3[7];
+    for (int i=0; i<7; i++){
+        cin>>arr3[i];
+    }
+    cout<<sum3(arr3);
+
+
+
+    
+
+    //Question 4
+    double Arr4[10];
+    for (int i=0; i<10; i++){
+        cin>>Arr4[i];
+    }
+    double num4;
+    cin>>num4;
+    if(check4(Arr4, num4)){
+        cout<<"The number "<<num4<<" is in an array"<<endl;
+    } else{
+        cout<<"The number "<<num4<<" is not in an array"<<endl;
+    }
+
+
+
+
+    //Question 5
+    double arr5[5];
+    for (int i=0; i<5; i++){
+        cin>>arr5[i];
+    }
+    pow2(arr5);
+    for (int i=0; i<4; i++){
+        cout<<arr5[i]<<" ";
+    }
+    cout<<arr5[4]<<endl;
 
 
 
